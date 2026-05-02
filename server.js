11e9e35ea13ea1081daf489bbb7774e24075db35
Deepika J <deepikajagadishd668@gmail.com>
2026-05-02 21:28:19 +0530

    Fix CORS

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const Groq = require("groq-sdk");
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 
 const upload = multer({ dest: "uploads/" });
